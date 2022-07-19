b1ae826ce04e8a9e481deff55bfceb1faf3bc0bd
Jyothiradithya-R <jyothiradithyaravula@gmail.com>
2022-07-19 16:35:55 +0530

    Practice Project

--- PracticeProject/ArithmeticCalculator.java
+++ PracticeProject/ArithmeticCalculator.java
@@ -8,14 +8,14 @@ public class ArithmeticCalculator {
 
 	    Scanner input = new Scanner(System.in);
 
-	    System.out.println("Choose an operator: +, -, *, or /");
-	    operator = input.next().charAt(0);
-
 	    System.out.println("Enter first number");
 	    number1 = input.nextDouble();
 
 	    System.out.println("Enter second number");
 	    number2 = input.nextDouble();
+	    
+	    System.out.println("Choose an operator: +, -, *, or /");
+	    operator = input.next().charAt(0);
 
 	    switch (operator) {
 
