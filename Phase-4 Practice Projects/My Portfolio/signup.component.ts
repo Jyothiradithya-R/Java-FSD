dcf96b91ce96282e1f779969659f8184cf355f4c
Jyothiradithya-R <109268637+Jyothiradithya-R@users.noreply.github.com>
2022-09-24 22:07:59 +0530

    Add files via upload

---
+++ Phase-4 Practice Projects/My Portfolio/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class signupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
